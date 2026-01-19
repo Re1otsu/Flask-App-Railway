95e4d8dfdf9812b0db8ac0671bf461882a263ed5
Re1otsu <lbs2005v@gmail.com>
2026-01-19 18:10:17 +0500

    test

--- fix_alembic.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import text
-from main import app, db
-
-with app.app_context():
-    db.session.execute(text("DELETE FROM alembic_version"))
-    db.session.commit()
-
-print("OK: alembic_version cleared")
